bb724f1b56c4008119867a210c2ddf07699b3597
BauerJustin <justin.bauer@mail.utoronto.ca>
2021-05-22 09:04:09 -0600

    Added loop

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import blockchain as bc
+import string
+import random
 
 blockchain = bc.BlockChain()
 
 print("***Mining Bizcoin about to start***")
 print(blockchain.chain)
 
-last_block = blockchain.latest_block
-last_proof_no = last_block.proof_no
-proof_no = blockchain.proof_of_work(last_proof_no)
+while (True):
+    last_block = blockchain.latest_block
+    last_proof_no = last_block.proof_no
+    proof_no = blockchain.proof_of_work(last_proof_no)
 
-blockchain.new_data(
-    sender="0",  #it implies that this node has created a new block
-    recipient="John Cena",  #let's send Quincy some coins!
-    quantity=
-    1,  #creating a new block (or identifying the proof number) is awarded with 1
-)
+    letters = string.ascii_letters
 
-last_hash = last_block.calculate_hash
-block = blockchain.construct_block(proof_no, last_hash)
+    blockchain.new_data(
+        sender="0",  #it implies that this node has created a new block
+        recipient=''.join(random.choice(letters) for i in range(5)),  #let's send Quincy some coins!
+        quantity=1,  #creating a new block (or identifying the proof number) is awarded with 1
+    )
 
-print("***Mining Bizcoin has been successful***")
-print(blockchain.chain)
+    last_hash = last_block.calculate_hash
+    block = blockchain.construct_block(proof_no, last_hash)
+
+    print("***Mining Bizcoin has been successful***")
+    print("Coins in circulation: " + str(block.index))
+    print(blockchain.chain)
